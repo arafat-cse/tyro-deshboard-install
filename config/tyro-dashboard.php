e5a69f45850e0cdba3983ffd2c055c3684c0af9e
Arafat <arafat.developer2008@gmail.com>
2026-06-22 00:52:55 +0600

    feat: Update application name to 'life decode' and disable example routes in configuration

--- config/tyro-dashboard.php
+++ config/tyro-dashboard.php
@@ -698,5 +698,5 @@ return [
     | and the example routes will be disabled.
     |
     */
-    'disable_examples' => env('TYRO_DASHBOARD_DISABLE_EXAMPLES', false),
+    'disable_examples' => env('TYRO_DASHBOARD_DISABLE_EXAMPLES', true),
 ];

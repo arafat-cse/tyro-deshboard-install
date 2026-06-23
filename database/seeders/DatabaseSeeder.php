9a8e5ef9b0b65a3e9bee02eb4c50e85e3bff67ea
Arafat <arafat.developer2008@gmail.com>
2026-06-23 09:55:51 +0600

    feat: Update DatabaseSeeder to create superadmin and admin users with roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use HasinHayder\Tyro\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -16,15 +17,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::updateOrCreate(
-            ['email' => 'test@example.com'],
+        // Create superadmin user
+        $superAdmin = User::updateOrCreate(
+            ['email' => 'super@lifedecode.com'],
             [
-                'name' => 'Test User',
-                'password' => Hash::make('password'),
+                'name' => 'Super Admin',
+                'password' => Hash::make('Abcd@1234'),
             ]
         );
+
+        // Assign super-admin role to the user
+        $superAdminRole = Role::where('slug', 'super-admin')->first();
+        if ($superAdminRole) {
+            $superAdmin->roles()->syncWithoutDetaching([$superAdminRole->id]);
+        }
+
+        // Create admin user
+        $admin = User::updateOrCreate(
+            ['email' => 'admin@lifedecode.com'],
+            [
+                'name' => 'Administrator',
+                'password' => Hash::make('Abcd@1234'),
+            ]
+        );
+
+        // Assign admin role to the user
+        $adminRole = Role::where('slug', 'admin')->first();
+        if ($adminRole) {
+            $admin->roles()->syncWithoutDetaching([$adminRole->id]);
+        }
 
         $this->call(DashboardPermissionSeeder::class);
         $this->call(LibrarySeeder::class);
